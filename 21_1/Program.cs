d3f9c0af53220f51590a294546f9f2dc5708840a
Marina Savina <fighter_61@bk.ru>
2022-11-24 21:33:58 +0300

    Вторая цифра 3х-значного числа

---
+++ 21_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Второй цифрой числа {Number} является {Number / 10 % 10} ");
+System.Console.WriteLine();
